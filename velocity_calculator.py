54ae860a1fd17f7cf2ccf08bfc5432a1adbb89e1
lancercarstars991 <129785266+lancercarstars991@users.noreply.github.com>
2023-04-18 19:00:49 +0300

    Create velocity_calculator.py

---
+++ velocity_calculator.py
@@ -0,0 +1,3 @@
+def calculate_velocity(initial_velocity, acceleration, time):
+    """Calculate final velocity given initial velocity, acceleration, and time."""
+    return initial_velocity + acceleration * time
